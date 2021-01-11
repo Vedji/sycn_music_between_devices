aaf95f9b4c5abaf74ae1466e65e6a0acb362ca43
Veji <45602069+Vedji@users.noreply.github.com>
2021-01-11 14:45:20 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,22 @@
-import socket
+import socket, pickle
 
-HOST = '192.168.1.48'  # The server's hostname or IP address
+HOST = '192.168.1.31'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
+
+data = {
+    'name': 'LarMoV - Пароль ТЕТРИАНДОХ.mp3',
+    'value': []
+}
+
+
+file = open('client/LarMoV - Пароль ТЕТРИАНДОХ.mp3', 'rb')
+
+k = True
+while k:
+    k = file.read(1000)
+    data['value'].append(k)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    s.sendall(pickle.dumps(data))
+s.close()
