900ca118e5c392adf794ec763edc66b9adbc02fc
Veji <45602069+Vedji@users.noreply.github.com>
2021-01-11 14:45:22 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
-import socket
+import socket, pickle
 
 HOST = ''  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+music = b''
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((HOST, PORT))
+s.listen()
+while True:
     conn, addr = s.accept()
-    with conn:
-        print('Connected by', addr)
-        while True:
-            data = conn.recv(1024)
-            conn.sendall(data)
+    print('Connected by', addr)
+    data = True
+    while data:
+        data = conn.recv(1024)
+        music += data
+    music = pickle.loads(music)
+    file = open('server/'+music['name'], 'wb')
+    for i in music['value']:
+        file.write(i)
+    file.close()
+    print('All')
