4f43080570b338e8a348d77191026cfec3066a7d
Veji <45602069+Vedji@users.noreply.github.com>
2021-01-11 21:16:45 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,39 @@
-import socket, pickle
+import socket
+import pickle
+import os
+import time
 
-HOST = ''  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+HOST = socket.gethostbyname(socket.gethostname())
+PORT = 65432
+PATHTOMUSIC = 'server\\'
 
 music = b''
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('Server ip: ', HOST)
 s.bind((HOST, PORT))
 s.listen()
 while True:
+    start_time = time.time()
+
     conn, addr = s.accept()
     print('Connected by', addr)
     data = True
     while data:
-        data = conn.recv(1024)
+        data = conn.recv(134217728)
         music += data
     music = pickle.loads(music)
-    file = open('server/'+music['name'], 'wb')
-    for i in music['value']:
-        file.write(i)
-    file.close()
-    print('All')
+    print('Get Music finished. ', end='')
+    print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    while music:
+        cmf = music.pop(0)
+        music_data = pickle.loads(cmf)
+        if not os.path.exists(PATHTOMUSIC + music_data['path']):
+            os.makedirs(PATHTOMUSIC + music_data['path'])
+        music_file = open(PATHTOMUSIC + music_data['path'] + music_data['name'], 'wb')
+        music_file.write(music_data['value'])
+    print(' Write music Finished. ', end='')
+    print("--- %s seconds ---" % (time.time() - start_time))
+    music = b''
+    print('Client {} processed. '.format(addr))
